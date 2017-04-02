cb5cd95e4eff92c1b5361e91d8145255d640feb8
Karpaga Ganesh Patchirajan <karpagaganesh_patchirajan@intuit.com>
2017-04-02 18:52:43 -0500

    table view setup

---
+++ Flicks/FlicksViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class FlicksViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self;
+        tableView.delegate = self;
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 5;
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath);
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        return cell;
+    }
+
+}
